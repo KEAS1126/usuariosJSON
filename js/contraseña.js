17e61d7e3d3264b492c67eb0ea5bc7de40e6c8e5
Kevin Estiven <keas11262000@gmail.com>
2022-12-07 10:23:26 -0500

    en rojo si tiene 4 o menos caracteres y en verde si tiene más

--- js/contraseña.js
+++ js/contraseña.js
@@ -1,7 +1,15 @@
 var data = JSON.parse(httpGet('https://randomuser.me/api/?results=250'));
 
 for(i=0;i<25;i++){
-    login=document.write("<p>"+data.results[i].login.password+"</p>")
+    
+    caracteres=JSON.stringify(data.results[i].login.password.length)
+    if(caracteres<=4){
+        login=document.write("<p style='color:red'>"+JSON.stringify(data.results[i].login.password)+"</p>")
+    }
+    else{
+        login=document.write("<p style='color:green'>"+JSON.stringify(data.results[i].login.password)+"</p>")
+    }
+    console.log(caracteres)
     document.getElementsByTagName("td").innerHTML=login
 }
 function httpGet(theUrl){
--- js/imagen.js
+++ js/imagen.js
@@ -1,8 +1,9 @@
 var data = JSON.parse(httpGet('https://randomuser.me/api/?results=250'));
 
 for(i=0;i<25;i++){
-    picture=document.write("<p>"+data.results[i].picture.large+"</p>")
+    picture=document.write("<p>"+JSON.stringify(data.results[i].picture.large)+"</p>")
     document.getElementsByTagName("td").innerHTML=picture
+
 }
 function httpGet(theUrl){
     var xmlHttp = new XMLHttpRequest();xmlHttp.open( "GET", theUrl, false );xmlHttp.send( null );
